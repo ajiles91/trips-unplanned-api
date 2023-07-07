a064fefdf1dac1f3f80895a126d869a89c0d2b11
Amber <ajiles91@gmail.com>
2023-07-07 15:45:40 -0500

    update gitignore

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/trips-unplanned'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://default:cMEp1tuvzma8@ep-flat-glade-672401-pooler.us-east-1.postgres.vercel-storage.com/verceldb'
 }
