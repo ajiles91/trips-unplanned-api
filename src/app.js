dc727b7937469e4ebfa935289110ba11089b81cc
Amber <ajiles91@gmail.com>
2023-07-23 23:25:06 -0500

    moving db

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const knex = require('knex')
-const { DATABASE_URL} = require('./config')
 const errorHandler = require('./error-handler')
 const commentsRouter = require('./comments-router')
 
@@ -14,13 +12,6 @@ const app = express()
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
-
-// const db = knex({
-//     client: 'pg',
-//     connection: DATABASE_URL,
-//   })
-
-// app.set('db', db)
 
 app.use(morgan(morganOption))
 app.use(helmet())
