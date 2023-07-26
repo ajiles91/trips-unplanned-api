1e394d4e1f8cfa5e120f06cab6126872618e4da1
Amber <ajiles91@gmail.com>
2023-07-25 20:19:01 -0500

    removed import from appjs

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { NODE_ENV } = require('./config')
 const errorHandler = require('./error-handler')
 const commentsRouter = require('./comments-router')
 const db = require("./data/db.js");
-import db from '../data/db.js'
+// import db from '../data/db.js'
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
